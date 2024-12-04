67f5d1b25587f53df39b620570e148fa870457e6
Aleksei Chesnokov <173167461+Aleksei-Che@users.noreply.github.com>
2024-12-04 22:24:52 +0100

    Ensure firebaseConfig.tsx is clean and uses environment variables

--- S7-star-wars/src/firebaseConfig.tsx
+++ S7-star-wars/src/firebaseConfig.tsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxjd1qVH32ipQg-fuKBRMcTr8HSl_lyZI",
-    authDomain: "s7-star-wars-74840.firebaseapp.com",
-    projectId: "s7-star-wars-74840",
-    storageBucket: "s7-star-wars-74840.firebasestorage.app",
-    messagingSenderId: "765738497821",
-    appId: "1:765738497821:web:19fc6be2addb75a3ef13f4"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
 
   const app = initializeApp(firebaseConfig);
